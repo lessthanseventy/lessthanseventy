c9569ecf12beeea2f8bd301d3c60396d53c0542d
Andrew Moore <andrew@lessthanseventy.com>
2021-02-04 20:21:58 -0700

    update styles for blog article list

--- src/components/blogComponents/blog-article-list.js
+++ src/components/blogComponents/blog-article-list.js
@@ -8,63 +8,72 @@ export default ({ data }) => (
     <h2 className="section-headline">Recent articles</h2>
     <ul className={styles.articleList}>
       {data.map(({ node }, index) => {
+        console.log(node)
         return (
           <li key={node.slug}>
             {index % 2 === 0 && (
-              <div
-                data-sal="flip-down"
-                data-sal-duration="500"
-                data-sal-easing="ease-in-cubic"
-                className={styles.imageWrapper}
-              >
-                <Link to={`/blog/${node.slug}`}>
-                  <Img alt="" fixed={node.heroImage.fixed} />
-                </Link>
-              </div>
+              <Link to={`/blog/${node.slug}`}>
+                <div
+                  data-sal="flip-down"
+                  data-sal-duration="500"
+                  data-sal-easing="ease-in-cubic"
+                  className={styles.imageWrapper}
+                >
+                  <Img
+                    alt=""
+                    fluid={node.heroImage.fluid}
+                    style={{
+                      width: '100%',
+                    }}
+                  />
+                </div>
+                <div
+                  data-sal="flip-left"
+                  data-sal-duration="500"
+                  data-sal-easing="ease-in-cubic"
+                  className={styles.descWrapper}
+                >
+                  <h3 className={styles.previewTitle}>{node.title}</h3>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: node.description.childMarkdownRemark.html,
+                    }}
+                  />
+                  <small>{node.publishDate}</small>
+                </div>
+              </Link>
             )}
             {index % 2 === 1 && (
-              <div
-                data-sal="slide-left"
-                data-sal-duration="500"
-                data-sal-easing="ease-out-sine"
-                className={styles.imageWrapper}
-              >
-                <Link to={`/blog/${node.slug}`}>
-                  <Img alt="" fixed={node.heroImage.fixed} />
-                </Link>
-              </div>
-            )}
-            {index % 2 === 0 && (
-              <div
-                data-sal="flip-left"
-                data-sal-duration="500"
-                data-sal-easing="ease-in-cubic"
-                className={styles.descWrapper}
-              >
-                <h3 className={styles.previewTitle}>{node.title}</h3>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.description.childMarkdownRemark.html,
-                  }}
-                />
-                <small>{node.publishDate}</small>
-              </div>
-            )}
-            {index % 2 === 1 && (
-              <div
-                data-sal="slide-left"
-                data-sal-duration="500"
-                data-sal-easing="ease-in-cubic"
-                className={styles.descWrapper}
-              >
-                <h3 className={styles.previewTitle}>{node.title}</h3>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.description.childMarkdownRemark.html,
-                  }}
-                />
-                <small>{node.publishDate}</small>
-              </div>
+              <Link to={`/blog/${node.slug}`}>
+                <div
+                  data-sal="slide-left"
+                  data-sal-duration="500"
+                  data-sal-easing="ease-out-sine"
+                  className={styles.imageWrapper}
+                >
+                  <Img
+                    alt=""
+                    fluid={node.heroImage.fluid}
+                    style={{
+                      width: '100%',
+                    }}
+                  />
+                </div>
+                <div
+                  data-sal="slide-left"
+                  data-sal-duration="500"
+                  data-sal-easing="ease-in-cubic"
+                  className={styles.descWrapper}
+                >
+                  <h3 className={styles.previewTitle}>{node.title}</h3>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: node.description.childMarkdownRemark.html,
+                    }}
+                  />
+                  <small>{node.publishDate}</small>
+                </div>
+              </Link>
             )}
           </li>
         )
